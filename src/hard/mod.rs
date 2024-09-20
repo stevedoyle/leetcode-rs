4fadaa0b5c0b6c63dbd2d094aa98fa326f90678b
Stephen Doyle <stephendoyle75@gmail.com>
2024-09-20 09:16:13 +0100

    Add P32 solution

--- src/hard/mod.rs
+++ src/hard/mod.rs
@@ -1,4 +1,5 @@
 mod p10_regular_expression_matching;
 mod p30_substring_with_concatenation_of_all_words;
+mod p32_longest_valid_parentheses;
 mod p41_first_missing_positive;
 mod p4_median_of_two_sorted_arrays;
