657b42f85551fb356031772e63725e1e37c786de
Stephen Doyle <stephendoyle75@gmail.com>
2024-09-26 13:13:37 +0100

    Fix P88 linter issues

--- src/easy/p88_merge_sorted_array.rs
+++ src/easy/p88_merge_sorted_array.rs
@@ -42,7 +42,7 @@ mod tests {
     fn test_example1() {
         let mut nums1 = vec![1, 2, 3, 0, 0, 0];
         let m = 3;
-        let mut nums2 = vec![2, 5, 6];
+        let nums2 = vec![2, 5, 6];
         let n = 3;
         Solution::merge(&mut nums1, m, &nums2, n);
         assert_eq!(nums1, vec![1, 2, 2, 3, 5, 6]);
@@ -52,7 +52,7 @@ mod tests {
     fn test_example2() {
         let mut nums1 = vec![1];
         let m = 1;
-        let mut nums2 = vec![];
+        let nums2 = vec![];
         let n = 0;
         Solution::merge(&mut nums1, m, &nums2, n);
         assert_eq!(nums1, vec![1]);
@@ -62,7 +62,7 @@ mod tests {
     fn test_example3() {
         let mut nums1 = vec![0];
         let m = 0;
-        let mut nums2 = vec![1];
+        let nums2 = vec![1];
         let n = 1;
         Solution::merge(&mut nums1, m, &nums2, n);
         assert_eq!(nums1, vec![1]);
